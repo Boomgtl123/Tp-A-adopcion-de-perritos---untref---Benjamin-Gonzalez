ad7a0dbf495e96ce64300a289eea38426e1c119f
Benjamin Gonzalez <boomdigital@MacBook-Air-Benja.local>
2025-12-18 14:10:23 -0500

    Add built files for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Tp-A-adopcion-de-perritos---untref---Benjamin-Gonzalez/',
+  build: {
+    outDir: 'docs'
+  }
 })
